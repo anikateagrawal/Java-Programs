883606b83daf4cf6eeb1410b1341a6640d62cd69
anikateagrawal <anikateag7316@gmail.com>
2021-09-25 20:41:21 +0530

    moved to package p1

--- src/p1/Sum.java
+++ src/p1/Sum.java
@@ -1,3 +1,4 @@
+package p1;
 import java.util.Scanner;
 
 public class Sum {
@@ -17,19 +18,19 @@ public class Sum {
     void sum()
     {
         int sum=0,f=0,s2=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]==6)
-            {
-                f=1;
+        for (int j : arr) {
+            if (j == 6) {
+                f = 1;
             }
-            if(f==1)
-            {
-                s2=s2+arr[i];
-                if(arr[i]==7){s2=0;f=0;}
-            }
-            else sum=sum+arr[i];
-        }sum=sum+s2;
+            if (f == 1) {
+                s2 = s2 + j;
+                if (j == 7) {
+                    s2 = 0;
+                    f = 0;
+                }
+            } else sum = sum + j;
+        }
+        sum=sum+s2;
         System.out.println(sum);
     }
 }
