2db3522dd18439fe35461c269708708ea456b940
anikateagrawal <anikateag7316@gmail.com>
2021-09-25 18:31:17 +0530

    Ascii to character

---
+++ src/Ascii.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ascii {
+    int[] arr;
+    Scanner s=new Scanner(System.in);
+    Ascii()
+    {
+        System.out.println("Enter size of array");
+        int size =s.nextInt();
+        arr=new int[size];
+        for(var i=0;i<size;i++)
+        {
+            System.out.println("Enter a ascii number");
+            arr[i]=s.nextInt();
+        }
+    }
+    void display()
+    {
+        for(var i:arr)
+        {
+            System.out.println((char)i);
+        }
+    }
+}
+class AsciiMain
+{
+    public static void main(String[] args) {
+        Ascii obj=new Ascii();
+        obj.display();
+    }
+}
