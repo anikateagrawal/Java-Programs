6f29bcd84a95a06c8379b404b859fd1505b41ae2
anikateagrawal <anikateag7316@gmail.com>
2021-11-30 19:37:08 +0530

    Java Program

--- src/jdbc/Statements.java
+++ src/jdbc/Statements.java
@@ -4,20 +4,16 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Statements {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/google_play_game", "root", "");
         Scanner sc=new Scanner(System.in);
         String name=sc.nextLine();
         Statement s = con.createStatement();
         ResultSet rs;
-        try {
-            rs = s.executeQuery("select * from emp where ename=" + name);
-            while (rs.next()) {
-                System.out.println(rs.getString(2) + " " + rs.getInt(4));
-            }
-        }catch (Exception e){
-            System.out.println("error");
+        rs = s.executeQuery("select * from emp where ename=" + name);
+        while (rs.next()) {
+            System.out.println(rs.getString(2) + " " + rs.getInt(4));
         }
         PreparedStatement ps=con.prepareStatement("SELECT * from emp where ename = ?");
         ps.setString(1,name);
---
+++ src/regular_expression/Phone_number.java
@@ -0,0 +1,24 @@
+package regular_expression;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Phone_number {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String target=sc.nextLine();
+        Pattern p=Pattern.compile("(0|91)?[7-9][0-9]{9}");
+        Pattern p1=Pattern.compile("[a]+\\S");
+        Matcher m=p1.matcher(target);
+        int c=0;
+        while(m.find())
+        {
+            c++;
+            System.out.println(m.start()+"  "+m.end()+"  "+m.group());
+        }
+        System.out.println(c);
+        String s1="9999555566";
+        System.out.println(s1.matches("(0|91)?[7-9][0-9]{9}"));
+    }
+}
