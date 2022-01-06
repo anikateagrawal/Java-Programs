5369aca1a5ae0829ba870c586bad8a15d7c24f7f
anikateagrawal <anikateag7316@gmail.com>
2022-01-06 17:18:41 +0530

    practical program

---
+++ src/spiral.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class spiral {
+    public static void main(String[] args) {
+        int m,n;
+        Scanner sc=new Scanner(System.in);
+        m=sc.nextInt();n= sc.nextInt();
+        int a[][]=new int[m][n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        int ar[]=new int [m*n];
+        int k=0,c=0,ir=0,ic=0,fr=m-1,fc=n-1;
+        while(c!=m*n)
+        {
+            if(k%4==0)
+            {
+                for(int i=ir;i<=fr;i++)
+                    ar[c++]=a[i][ic];
+                ic++;
+                System.out.println("first");
+            }
+            else if(k%4==1)
+            {
+                for(int i=ic;i<=fc;i++)
+                    ar[c++]=a[fr][i];
+                fr--;
+                System.out.println("Second");
+            }
+            else if(k%4==2)
+            {
+                for(int i=fr;i>=ir;i--)
+                    ar[c++]=a[i][fc];
+                fc--;
+                System.out.println("Third");
+            }
+            else
+            {
+                for(int i=fc;i>=ic;i--)
+                    ar[c++]=a[ir][i];
+                ir++;
+                System.out.println("Fourth");
+            }
+            k++;
+        }
+        for(int i=0;i<m*n;i++)
+        {
+            System.out.print(ar[i]+", ");
+        }
+        System.out.println("End");
+
+    }
+}
